d6a2fe2b727c17c751746a745cedbea77e03c91a
Burlakov Alexander <alkboor@mail.ru>
2026-07-25 13:55:58 +0000

    commit 3

---
+++ internal/app/schema.go
@@ -0,0 +1,19 @@
+package app
+
+import "database/sql"
+
+func InitSchema(db *sql.DB) error {
+
+	const schema = `
+CREATE TABLE IF NOT EXISTS app (
+    id INTEGER PRIMARY KEY CHECK (id = 1),
+    version INTEGER NOT NULL
+);
+
+INSERT OR IGNORE INTO app(id, version)
+VALUES (1, 1);
+`
+
+	_, err := db.Exec(schema)
+	return err
+}
