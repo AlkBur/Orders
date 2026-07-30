36e9e47fc0081cffc8806c1223c156af314842ed
Burlakov Alexander <alkboor@mail.ru>
2026-07-30 20:36:46 +0000

    bukma

--- internal/app/assets_release.go
+++ internal/app/assets_release.go
@@ -11,6 +11,11 @@ import (
 //go:embed templates/*
 var templatesFS embed.FS
 
+//go:embed templates/layout
+//go:embed templates/components
+//go:embed templates/pages
+var showcaseTemplatesFS embed.FS
+
 //go:embed static/**
 var staticFS embed.FS
 
@@ -44,6 +49,14 @@ func LoadTemplates(page string) (*template.Template, error) {
 
 func StaticFS() fs.FS {
 	sub, err := fs.Sub(staticFS, "static")
+	if err != nil {
+		panic(err)
+	}
+	return sub
+}
+
+func ShowcaseFS() fs.FS {
+	sub, err := fs.Sub(showcaseTemplatesFS, "templates")
 	if err != nil {
 		panic(err)
 	}
