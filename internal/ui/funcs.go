27193429aea63738f87c8eaa5c854211bae71090
Burlakov Alexander <alkboor@mail.ru>
2026-08-01 13:56:17 +0000

    display: FormatNumber with thousands separators
    Universal integer formatter (space as thousands separator) exposed to
    all templates as formatNumber. The IntegerNumber constraint keeps it to
    integer kinds; cmp.Ordered is too wide (floats/strings have no digit
    grouping).

--- internal/ui/funcs.go
+++ internal/ui/funcs.go
@@ -1,12 +1,17 @@
 package ui
 
-import "html/template"
+import (
+	"html/template"
+
+	"Orders/internal/ui/display"
+)
 
 // Funcs returns the shared template functions of the platform.
 // Every page template is parsed with these functions.
 func Funcs() template.FuncMap {
 	return template.FuncMap{
-		"icon":   icon,
-		"getFAB": getFAB,
+		"icon":         icon,
+		"getFAB":       getFAB,
+		"formatNumber": func(v int64) string { return display.FormatNumber(v) },
 	}
 }
